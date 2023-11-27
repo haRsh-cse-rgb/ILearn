4c6834e19a6638404ff1c356a584a9a3e014b48a
harsh <rockingharsh305@gmail.com>
2023-11-27 16:07:58 +0530

    c

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ config({
 })
 
 const corsOptions = {
-    origin: 'https://ilearn-drab.vercel.app/', // Change this to the actual origin of your frontend app
+    origin: 'https://ilearn-drab.vercel.app', // Change this to the actual origin of your frontend app
     credentials: true,
     methods:["GET" , "POST" , "PUT" , "DELETE"]
   };
